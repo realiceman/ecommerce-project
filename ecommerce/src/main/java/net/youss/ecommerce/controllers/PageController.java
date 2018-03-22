cfd4417f530f71242dd2ed6adc70adf19647b1e5
youss <youss.hark@gmail.com>
2018-03-22 17:04:15 -0400

    completed the interface theme

--- ecommerce/src/main/java/net/youss/ecommerce/controllers/PageController.java
+++ ecommerce/src/main/java/net/youss/ecommerce/controllers/PageController.java
@@ -12,27 +12,27 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	
-	@RequestMapping(value = "/test")
-	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
-		if(greeting==null) {
-			greeting="titi";
-		}
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
 	}
 	
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test2(@PathVariable("greeting")String greeting) {
-		if(greeting==null) {
-			greeting="titi";
-		}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","Contact");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
+
 }
---
+++ ecommerce/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+   
+       <h1>About Us</h1>
+       <hr/>
+       <p>This is an about us page</p>
+ 
+</div>
